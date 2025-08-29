ce5209b76c8464a9dbdd49f94b702cacacb077f0
Caio Schmidt <caiofonseca6@gmail.com>
2025-08-29 11:03:30 -0300

    Adiciona captação de erro 401 / Unauthorized para o erro de domínio InvalidCredentials

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -108,4 +108,24 @@ void main() {
       );
     },
   );
+
+  test(
+    'Should throw InvalidCredentialsError if HttpClient returns 401',
+    () async {
+      when(
+        httpClient.request(
+          url: anyNamed('url'),
+          method: anyNamed('method'),
+          body: anyNamed('body'),
+        ),
+      ).thenThrow(HttpError.unauthorized);
+
+      final future = sut.auth(params);
+
+      expect(
+        future,
+        throwsA(DomainError.invalidCredentials),
+      );
+    },
+  );
 }
