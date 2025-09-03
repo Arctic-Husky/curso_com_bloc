2362eb695a18253c229e22fc9c5863dd28a7de06
Caio Schmidt <caiofonseca6@gmail.com>
2025-09-03 10:44:10 -0300

    Adiciona case e teste post para HttpAdapter

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,45 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
+// group('post', () {
+//   test('', () {});
+// });
+
+import 'http_adapter_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<Client>()])
+void main() {
+  group('post', () {
+    test(
+      'Should call post with correct values',
+      () async {
+        // Etapa Arrange
+        final client = MockClient();
+        final sut = HttpAdapter(client);
+        final url = faker.internet.httpUrl();
+
+        // Etapa Act
+        await sut.request(url: url, method: 'post');
+
+        verify(client.post(Uri.parse(url)));
+      },
+    );
+  });
+}
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    required String url,
+    required String method,
+    Map? body,
+  }) async {
+    await client.post(Uri.parse(url));
+  }
+}
